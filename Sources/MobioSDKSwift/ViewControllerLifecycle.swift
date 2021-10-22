93fcc92068ae98c7e35803a752011ae6c7ff1324
LinhNobi <linhtn@mobio.vn>
2021-10-22 09:23:23 +0700

    edit track time start/ end

--- Sources/MobioSDKSwift/ViewControllerLifecycle.swift
+++ Sources/MobioSDKSwift/ViewControllerLifecycle.swift
@@ -79,14 +79,17 @@ public extension UIViewController {
             super.viewWillDisappear(animated)
             applyBehaviors { $0.beforeDisappearing($1) }
             
-            print("End : \(Date())")
-            screenEnd(eventKey: "sdk_mobile_test_screen_end_in_app")
+            let screenName = UserDefaults.standard.string(forKey: "m_screen_current_view")
+            UserDefaults.standard.set(screenName, forKey: "m_screen_exit_view")
+            UserDefaults.standard.synchronize()
+
         }
         
         public override func viewDidDisappear(_ animated: Bool) {
             super.viewDidDisappear(animated)
             applyBehaviors { $0.afterDisappearing($1) }
             print("viewDidDisappear")
+            screenEnd(eventKey: "sdk_mobile_test_screen_end_in_app")
             self.timer?.invalidate()
         }
         
